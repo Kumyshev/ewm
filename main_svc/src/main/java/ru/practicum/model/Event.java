d23774cf9776dfe1ac64b8ee844921d7c417c4ce
akhmed <44803512+Kumyshev@users.noreply.github.com>
2024-10-15 17:17:52 +0300

    Update Event.java

--- main_svc/src/main/java/ru/practicum/model/Event.java
+++ main_svc/src/main/java/ru/practicum/model/Event.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Size(min = 20, max = 2000)
     private String annotation;
     @ManyToOne
     private Category category;
@@ -28,6 +30,7 @@ public class Event {
     private Integer confirmedRequests;
     @Column(name = "created_on")
     private String createdOn;
+    @Size(min = 20, max = 7000)
     private String description;
     @Column(name = "event_date")
     private String eventDate;
