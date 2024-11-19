7623239d6c9eba41b46ff1929b7ffe5eb7015d57
akhmed <44803512+Kumyshev@users.noreply.github.com>
2024-11-20 01:03:45 +0300

    Update HitsRepository.java

--- stat_svc/http_service/src/main/java/ru/practicum/repository/HitsRepository.java
+++ stat_svc/http_service/src/main/java/ru/practicum/repository/HitsRepository.java
@@ -16,14 +16,14 @@ public interface HitsRepository extends JpaRepository<EndpointHit, Long> {
                         "from hits s " +
                         "where s.timestamp between :start and :end " +
                         "group by s.app, s.uri " +
-                        "order by i desc")
+                        "order by count(distinct s.ip) desc")
         List<ViewStatsDto> getUniqueStats(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
         @Query("select new ru.practicum.ViewStatsDto(s.app, s.uri, count(s.ip) as i) " +
                         "from hits s " +
                         "where s.timestamp between :start and :end " +
                         "group by s.app, s.uri " +
-                        "order by i desc")
+                        "order by count(s.ip) desc")
         List<ViewStatsDto> getStats(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
         @Query("select new ru.practicum.ViewStatsDto(s.app, s.uri, count(distinct s.ip) as i) " +
@@ -31,7 +31,7 @@ public interface HitsRepository extends JpaRepository<EndpointHit, Long> {
                         "where s.timestamp between :start and :end " +
                         "and s.uri in (:uris) " +
                         "group by s.app, s.uri " +
-                        "order by i desc")
+                        "order by count(distinct s.ip) desc")
         List<ViewStatsDto> getUniqueStatsByUris(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end,
                         @Param("uris") List<String> uris);
 
@@ -40,7 +40,7 @@ public interface HitsRepository extends JpaRepository<EndpointHit, Long> {
                         "where s.timestamp between :start and :end " +
                         "and s.uri in (:uris) " +
                         "group by s.app, s.uri " +
-                        "order by i desc")
+                        "order by count(s.ip) desc")
         List<ViewStatsDto> getStatsByUris(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end,
                         @Param("uris") List<String> uris);
 
